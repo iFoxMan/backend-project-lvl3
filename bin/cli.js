3e50a577d2fdc4be65b61d38eea82b2c7a4e630e
Ivan Lisin <32236977+iFoxMan@users.noreply.github.com>
2020-10-20 20:25:51 +0700

    add cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-// import pageLoader from '../index';
+// TODO: resolve import description and version from package.json
+// Error: Unknown file extension ".json" for /<path>/backend-project-lvl3/package.json
+import { program } from 'commander';
+// import { description, version } from '../package.json';
+import pageLoader from '../src/page-loader.js';
+
+program
+  .arguments('<url>')
+  // .description(description)
+  // .version(version)
+  .option('--output [dirpath]', 'output directory', '.')
+  .action((url) => {
+    try {
+      console.log(pageLoader(url, program.output));
+    } catch (err) {
+      console.log(err.message);
+    }
+  })
+  .parse(process.argv);
