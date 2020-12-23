ccd4cf798848f3039ab4610abe83e27cc85d9d45
Ivan Lisin <32236977+iFoxMan@users.noreply.github.com>
2020-12-23 16:59:42 +0700

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,24 @@
-// eslint-disable-next-line import/prefer-default-export
-export const convertUrlToBaseName = (url) => url
-  .replace(/.+:\/\//, '')
-  .replace(/\/$/, '')
-  .replace(/[^a-zA-Z0-9]+\b/gm, '-');
+import _ from 'lodash';
+import path from 'path';
+
+const converUrlToKebabCase = (url, base = null, extname = '') => {
+  const normalizedBase = base === null ? _.noop() : base;
+  return new URL(url, normalizedBase).href
+    .replace(/.+:\/\//, '')
+    .replace(/\/$/, '')
+    .replace(extname, '')
+    .replace(/[^a-zA-Z0-9]+\b/gm, '-');
+};
+
+export const makeFileNameByUrl = (url, base = null) => {
+  const extname = path.extname(url);
+  const basename = converUrlToKebabCase(url, base, extname);
+  return extname === ''
+    ? `${basename}.html`
+    : `${basename}${extname}`;
+};
+
+export const makeDirNameByUrl = (url, base = null) => {
+  const dirname = converUrlToKebabCase(url, base);
+  return `${dirname}_files`;
+};
