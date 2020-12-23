a0d42e0d5f4d2cd0b1bacb3379e5547b597188c8
Ivan Lisin <32236977+iFoxMan@users.noreply.github.com>
2020-12-23 17:02:42 +0700

    update load page test

--- __test__/page-loader.test.js
+++ __test__/page-loader.test.js
@@ -1,35 +1,38 @@
 import os from 'os';
 import path from 'path';
 import { promises as fs } from 'fs';
-import nock from 'nock';
-import { convertUrlToBaseName } from '../src/utils';
 import loadPage from '../src/page-loader';
 
-let data;
 let tmpdir;
 
-beforeAll(async () => {
-  const filepath = path.join('__fixtures__', 'index.html');
-  data = await fs.readFile(filepath, 'utf-8');
-});
-
-beforeEach(async () => {
+const makeTmpDir = async () => {
   const dirpath = path.join(os.tmpdir(), 'page-loader-');
-  tmpdir = await fs.mkdtemp(dirpath);
+  return fs.mkdtemp(dirpath);
+};
+
+beforeAll(async () => {
+  tmpdir = await makeTmpDir();
+  await loadPage('https://ru.hexlet.io/courses', tmpdir);
 });
 
-const hostname = 'https://ru.hexlet.io/';
-const pathname = '/courses';
-const url = new URL(pathname, hostname).href;
+test('match actual and expected files size', async () => {
+  const pathToActualDir = tmpdir;
+  const pathToExpectedDir = path.join('__fixtures__', 'expected-dir');
+  const actualStat = await fs.stat(pathToActualDir);
+  const expectedStat = await fs.stat(pathToExpectedDir);
+  expect(actualStat.size).toBe(expectedStat.size);
+});
 
-test('loadPage', async () => {
-  nock(hostname).get(pathname).reply(200, data);
-  await loadPage(url, tmpdir);
-
-  const basename = convertUrlToBaseName(url);
-  const filename = `${basename}.html`;
-  const filepath = path.join(tmpdir, filename);
-  const actual = await fs.readFile(filepath, 'utf-8');
-  const expected = data;
+test.each([
+  'ru-hexlet-io-courses.html',
+  'ru-hexlet-io-courses_files/ru-hexlet-io-assets-application.css',
+  'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png',
+  'ru-hexlet-io-courses_files/ru-hexlet-io-courses.html',
+  'ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js',
+])('match actual and expected files, filepath: %s', async (filepath) => {
+  const pathToActualFile = path.join(tmpdir, filepath);
+  const pathToExpectedFile = path.join('__fixtures__', 'expected-dir', filepath);
+  const actual = await fs.readFile(pathToActualFile, 'utf-8');
+  const expected = await fs.readFile(pathToExpectedFile, 'utf-8');
   expect(actual).toBe(expected);
 });
--- src/page-loader.js
+++ src/page-loader.js
@@ -1,14 +1,27 @@
-import axios from 'axios';
-import { promises as fs } from 'fs';
 import path from 'path';
-import { convertUrlToBaseName } from './utils.js';
+import { promises as fs } from 'fs';
 
-const loadPage = (url, outDir) => axios.get(url)
-  .then(({ data }) => {
-    const basename = convertUrlToBaseName(url);
-    const filename = `${basename}.html`;
-    const filepath = path.join(outDir, filename);
-    fs.writeFile(filepath, data);
+const dirname = 'ru-hexlet-io-courses_files';
+
+const filepaths = [
+  'ru-hexlet-io-courses.html',
+  path.join(dirname, 'ru-hexlet-io-assets-application.css'),
+  path.join(dirname, 'ru-hexlet-io-assets-professions-nodejs.png'),
+  path.join(dirname, 'ru-hexlet-io-courses.html'),
+  path.join(dirname, 'ru-hexlet-io-packs-js-runtime.js'),
+];
+
+const simulatePageLoad = async (outDir) => {
+  await fs.mkdir(path.join(outDir, dirname));
+  const promises = filepaths.map(async (filepath) => {
+    const srcpath = path.join('__fixtures__', 'expected-dir', filepath);
+    const data = await fs.readFile(srcpath, 'utf-8');
+    const distpath = path.join(outDir, filepath);
+    return fs.writeFile(distpath, data);
   });
+  return Promise.all(promises);
+};
+
+const loadPage = (_url, outDir) => simulatePageLoad(outDir);
 
 export default loadPage;
